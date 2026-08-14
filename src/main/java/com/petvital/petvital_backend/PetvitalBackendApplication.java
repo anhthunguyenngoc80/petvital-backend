9d81c8b4574a5984dd16b6b0e8e45e7147b8dc1f
anhthunguyenngoc80 <anhthunguyenngoc80@gmail.com>
2026-08-14 17:43:01 +0700

    Initial commit

---
+++ src/main/java/com/petvital/petvital_backend/PetvitalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.petvital.petvital_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetvitalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetvitalBackendApplication.class, args);
+	}
+
+}
